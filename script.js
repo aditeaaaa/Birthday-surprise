3e4c4336a9459c7500a894c98f2feb8723ea6cfd
aditeaaaa <aditisachin2016@gmail.com>
2026-07-30 16:35:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-setTimeout(()=>{
+setTimeout(() => {
 
-document.getElementById("loading").style.display="none";
+document.getElementById("loading").style.display = "none";
 
 document.getElementById("welcome").classList.remove("hidden");
 
 },2500);
 
+
 document.getElementById("continueBtn").onclick=function(){
 
-alert("Next screen coming in our next update 😊");
+document.getElementById("welcome").classList.add("hidden");
+
+document.getElementById("choice").classList.remove("hidden");
+
+}
+
+
+document.getElementById("sunflower").onclick=function(){
+
+alert("🌻 Sunflower story coming next!");
+
+}
+
+document.getElementById("lily").onclick=function(){
+
+alert("🤍 Lily story coming next!");
+
+}
+
+document.getElementById("dog").onclick=function(){
+
+alert("🐶 Golden Retriever story coming next!");
 
 }
