05b8c3fd62383159ec365ad75f2e6cc440890fa0
aditeaaaa <aditisachin2016@gmail.com>
2026-07-30 16:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Birthday Surprise Script
+setTimeout(()=>{
+
+document.getElementById("loading").style.display="none";
+
+document.getElementById("welcome").classList.remove("hidden");
+
+},2500);
+
+document.getElementById("continueBtn").onclick=function(){
+
+alert("Next screen coming in our next update 😊");
+
+}
